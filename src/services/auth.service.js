12b60e6e803c1e603ee80265b2ddff06b0cba28b
Dhiraj sawariyastore84@gmail.com <sawariyastore84@gmail.com>
2023-03-07 23:39:22 +0530

    Change AWS URL

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth/";
+const API_URL = "http://inventorymanagement-env-1.eba-chx7eghp.us-east-2.elasticbeanstalk.com/api/auth/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:5000/api/test/";
+const API_URL = "http://inventorymanagement-env-1.eba-chx7eghp.us-east-2.elasticbeanstalk.com/api/test/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
